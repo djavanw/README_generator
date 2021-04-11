36a15f6670705f5e7e6e0c922c575433d5582c4c
Javan <71285290+djavanw@users.noreply.github.com>
2021-04-11 17:59:56 -0600

    Cleaned up some of the comments

--- mdUtils/generateMarkdown.js
+++ mdUtils/generateMarkdown.js
@@ -70,7 +70,7 @@ function renderLicenseLink(license) {
   //(${currentLicense.url})
 }
 
-// TODO: Create a function that returns the license section of README
+// A function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   currentLicense= licenseObject.find(element => {
@@ -82,7 +82,7 @@ function renderLicenseSection(license) {
 }
 
 
-// TODO: Create a function to generate markdown for README
+// A function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
